2d8240a216bd57e7a104dd821d011d7d6c128476
Brando637 <59592256+Brando637@users.noreply.github.com>
2020-12-05 19:59:57 -0500

    Create AUP Policy and Full User Page
    Created and filled out AUP policy. In addition, created a page for access to users that have logged in. Currently, there is simple button to bring you to that page.

--- webApp/src/app/home-full/home-full.component.ts
+++ webApp/src/app/home-full/home-full.component.ts
@@ -1,15 +1,154 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { HttpCallsService } from '../http-calls.service';
 
 @Component({
   selector: 'app-home-full',
   templateUrl: './home-full.component.html',
-  styleUrls: ['./home-full.component.css']
+  styleUrls: ['./home-full.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeFullComponent implements OnInit {
 
-  constructor() { }
+  deleteScheduleForm: FormGroup;
+  searchCourseForm: FormGroup;
+  searchCourseFormKey: FormGroup;
 
-  ngOnInit(): void {
+  htmlToAdd:SafeHtml;
+  htmlToAddFull:SafeHtml;
+  responseHolder: object;
+  enable = new FormControl();
+  public limited: boolean = true;
+  public full: boolean = false;
+
+  onChange(ob: MatSlideToggleChange){
+    console.log(ob.checked);
+    if(ob.checked==true)
+    {
+      this.expand();
+    }
+    if(ob.checked==false)
+    {
+      this.collapse();
+    }
   }
 
+  constructor(private fb: FormBuilder, private appService: HttpCallsService, private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.searchCourseForm = this.fb.group({
+      subject:"",
+      courseNumber:""
+    });
+    this.searchCourseFormKey = this.fb.group({
+      keyword: new FormControl( '', [Validators.minLength(4)] )
+    });
+    this.deleteScheduleForm = this.fb.group({
+      deleteSchedule:""
+    });
+  }
+
+  expand(): void {
+    this.limited = false;
+    this.full = true;
+  }
+  collapse(): void{
+    this.limited = true;
+    this.full = false;
+  }
+
+  onDeleteSchedule(): void {
+    console.log(this.deleteScheduleForm.value);
+    this.appService.deleteSchedule(this.deleteScheduleForm.value.deleteSchedule).subscribe(
+      (response) => {
+        console.log("The response from the server is " + response)
+        this.htmlToAdd = '<h2>'+response.toString()+'</h2>';
+      },
+      (error) => console.log("The error returned from the server is" + error)
+    );
+  }
+
+  onSubmitCourse(): void {
+    console.log(this.searchCourseForm);
+    console.log(this.searchCourseForm.value);
+    this.appService.searchCourse(this.searchCourseForm.value).subscribe(
+      (response) => {
+        var attachHTML = "";
+        attachHTML = this.parseResultLimited(response);
+        this.htmlToAdd = this.sanitizer.bypassSecurityTrustHtml("<ul class=scheduleList>" + attachHTML + '</ul>');
+        attachHTML = this.parseResultFull(response);
+        this.htmlToAddFull = this.sanitizer.bypassSecurityTrustHtml("<ul class=scheduleList>" + attachHTML + '</ul>');
+      },
+      (error) => console.log("The error returned from the server is" + error)
+    );
+  }
+
+
+  onSubmitKey(): void{
+    this.appService.searchCourseKeyword(this.searchCourseFormKey.value).subscribe(
+      (response) =>{
+        var attachHTML = "";
+        attachHTML = this.parseResultLimited(response);
+        this.htmlToAdd = this.sanitizer.bypassSecurityTrustHtml("<ul class=scheduleList>" + attachHTML + '</ul>');
+        attachHTML = this.parseResultFull(response);
+        this.htmlToAddFull = this.sanitizer.bypassSecurityTrustHtml("<ul class=scheduleList>" + attachHTML + '</ul>');
+      },
+      (error) => {console.log("The error return from the server is" + error)}
+    );
+  }
+
+  onPublicSchedules(): void{
+    this.appService.getSchedules().subscribe(
+      (response) => {
+
+      },
+      (error) => {}
+    );
+  }
+
+  parseResultLimited(response): string{
+    var attachHTML = ""
+
+    if (response.includes("Sorry")) 
+    {
+      this.htmlToAdd = response;
+    }
+    else 
+    {
+      for (let x in response) 
+      {
+        console.log(response[x]);
+        attachHTML += '<h2>' + response[x].subject + ' - ' + response[x].className + ' ' + response[x].catalog_nbr + '</h2>';
+        attachHTML += '<div class=descrip>' + response[x].catalog_description + '</div>';
+        attachHTML += '<div' + response[x].course_info[0].ssr_component + '>' + response[x].course_info[0].ssr_component + '</div>';
+      }
+    }
+    return attachHTML;
+  }
+
+  parseResultFull(response): string {
+    var attachHTML = ""
+
+    if (response.includes("Sorry"))
+        {
+          this.htmlToAdd = response;
+        }
+        else
+        {
+          for (let x in response)
+          {
+            attachHTML += '<h2>' + response[x].subject + '' + response[x].catalog_nbr + ' - ' + response[x].className +  '</h2>';
+            attachHTML += '<div> Course Description: ' + response[x].catalog_description + '<div class=grid-container><div>Section</div><div>Component</div><div>Class Nbr</div><div>Days</div><div>Start Time</div><div>End Time</div><div>Location</div><div>Instructor</div><div>Requisites and Constraints</div><div>Status</div><div>Campus</div><div>Extra Info</div>';
+            attachHTML += '<div>'+response[x].course_info[0].class_section+'</div>' + '<div>'+response[x].course_info[0].ssr_component+'</div>' + '<div>'+response[x].course_info[0].class_nbr+'</div>'+'<div>'+response[x].course_info[0].days+'</div>'+'<div>'+response[x].course_info[0].start_time+'</div>'+'<div>'+response[x].course_info[0].end_time+'</div>'+'<div>'+response[x].course_info[0].facility_ID+'</div>'+'<div>'+response[x].course_info[0].instructors+'</div>'+'<div>'+response[x].course_info[0].descrlong+'</div>'+'<div>'+response[x].course_info[0].enrl_stat+'</div>'+'<div>'+response[x].course_info[0].campus+'</div>'+'<div>'+response[x].course_info[0].descr+'</div></div>';
+          }
+          attachHTML += '</div>';
+        }
+    return attachHTML;
+  }
 }
